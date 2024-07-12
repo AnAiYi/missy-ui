eedcd76ec27b3dd5f000d5de36a062488b5c8af2
wangzhihui <2878459042@qq.com>
2024-07-12 10:22:56 +0800

    init commit

---
+++ packages/components/es/packages/components/src/button/index.d.ts
@@ -0,0 +1,5 @@
+import { Plugin } from 'vue';
+
+type SFCWithInstall<T> = T & Plugin;
+declare const Button: SFCWithInstall<import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{}>>, {}, {}>>;
+export default Button;
---
+++ packages/components/es/packages/components/src/button/type.d.ts
@@ -0,0 +1,15 @@
+import { ExtractPropTypes } from 'vue';
+
+export declare const ButtonType: string[];
+export declare const ButtonSize: string[];
+export declare const buttonProps: {
+    type: {
+        type: StringConstructor;
+        values: string[];
+    };
+    size: {
+        type: StringConstructor;
+        values: string[];
+    };
+};
+export type ButtonProps = ExtractPropTypes<typeof buttonProps>;
